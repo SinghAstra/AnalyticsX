a61bc3d81d7f9f1cbac7eaee19074960fdc64c35
Astra <abhaypratapsinghwd@gmail.com>
2024-07-18 19:27:26 +0530

    basic app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,5 @@
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
-import RootLayout from "./layout/RootLayout";
-import About from "./pages/About";
-import ContactUs from "./pages/ContactUs";
-import Faq from "./pages/Faq";
-import Help from "./pages/Help";
-import Home from "./pages/Home";
-
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<RootLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="help" element={<Help />}>
-          <Route path="faq" element={<Faq />} />
-          <Route path="contact-us" element={<ContactUs />} />
-        </Route>
-      </Route>
-    )
-  );
-  return <RouterProvider router={router} />;
+  return <h1>App</h1>;
 }
 
 export default App;
