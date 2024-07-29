ec27ff865d6079633470203fc9e1f87d9b05882b
Astra <abhaypratapsinghwd@gmail.com>
2024-07-29 05:53:22 +0530

    Register User

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Register from "./pages/Register";
+
 function App() {
-  return <h1>App</h1>;
+  return (
+    <Routes>
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/" element={<Register />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/Register.js
@@ -0,0 +1,91 @@
+import axios from "axios";
+import React, { useState } from "react";
+import "../styles/Register.css";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    firstName: "The",
+    lastName: "Engineer",
+    location: "Human World",
+    occupation: "Coding",
+    picture: null,
+    email: "engine@gmail.com",
+    password: "123",
+  });
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    setFormData({
+      ...formData,
+      [name]: files ? files[0] : value,
+    });
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const formDataToSend = new FormData();
+    Object.keys(formData).forEach((key) => {
+      formDataToSend.append(key, formData[key]);
+    });
+    const res = await axios.post(
+      "http://localhost:5000/auth/register",
+      formDataToSend
+    );
+    console.log("res is ", res);
+    console.log("res.data is ", res.data);
+  };
+
+  return (
+    <div className="register-form">
+      <h2>Register</h2>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          name="firstName"
+          placeholder="First Name"
+          value={formData.firstName}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Last Name"
+          value={formData.lastName}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="location"
+          placeholder="Location"
+          value={formData.location}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="occupation"
+          placeholder="Occupation"
+          value={formData.occupation}
+          onChange={handleChange}
+        />
+        <input type="file" name="picture" onChange={handleChange} />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
