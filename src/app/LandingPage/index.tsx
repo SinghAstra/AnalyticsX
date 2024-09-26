5fffeb009cf0a92d1818ccbcea35e4ba210baac3
Astra <abhaypratapsinghwd@gmail.com>
2024-09-26 22:04:30 +0530

    LandingPage

---
+++ src/app/LandingPage/index.tsx
@@ -0,0 +1,21 @@
+import { BackgroundBeams } from "@/components/ui/background-beams";
+import React from "react";
+import FormGenerator from "../FormGenerator";
+
+const LandingPage = () => {
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <BackgroundBeams />
+      <h1 className="text-3xl text-center tracking-tighter sm:text-3xl md:text-6xl leading-6">
+        Streamline Your Form Creation Process!
+      </h1>
+      <p className="max-w-[600px] text-center text-gray-500 md:text-xl sm:text-sm">
+        Effortlessly create, customize, and manage forms with AI. Dive into
+        powerful analytics and insights with ease.
+      </p>
+      <FormGenerator />
+    </div>
+  );
+};
+
+export default LandingPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 import Header from "@/components/ui/header";
 import { SessionProvider } from "next-auth/react";
+import LandingPage from "./LandingPage/index";
 
 export default function Home() {
   return (
     <SessionProvider>
-      <Header />
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="relative p-24 z-10 antialiased flex-1 flex items-center justify-center">
+          <LandingPage />
+        </main>
+      </div>
     </SessionProvider>
   );
 }
