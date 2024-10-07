3147d0c9b8da74ac43baf6f9308661170dac53c8
Astra <abhaypratapsinghwd@gmail.com>
2024-10-07 17:10:00 +0530

    Added Home Landing Page

---
+++ app/(home)/GetStarted.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { Icons } from "@/components/Icons";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
+import { signIn } from "next-auth/react";
+import React, { useContext } from "react";
+
+const initialState: {
+  status: string;
+  message: string;
+  data?: any;
+} = {
+  status: "",
+  message: "",
+};
+
+const GetStarted = () => {
+  const { toast } = useToast();
+
+  // const handleGetStarted = () => {
+  //   if (session?.user) {
+  //     toast({ description: "You are authenticated" });
+  //   } else {
+  //     signIn();
+  //   }
+  // };
+
+  // return isAuthenticating ? (
+  //   <Button variant="outline" size="lg">
+  //     <Icons.spinner className="animate-spin mr-2" />
+  //     Wait...
+  //   </Button>
+  // ) : (
+  //   <Button onClick={handleGetStarted} className="cursor-pointer lg">
+  //     Get Started
+  //   </Button>
+  // );
+
+  return <Button className="cursor-pointer lg">Get Started</Button>;
+};
+
+export default GetStarted;
