65ebdce4820d2ebba8a496ab65ac01a3a7800cc9
Astra <abhaypratapsinghwd@gmail.com>
2024-10-09 08:32:16 +0530

    Rendering Post Author

---
+++ app/(home)/blog/[...slug]/page.tsx
@@ -0,0 +1,157 @@
+import { Icons } from "@/components/Icons";
+import MdxSection from "@/components/MdxSection";
+import { buttonVariants } from "@/components/ui/button";
+import { siteConfig } from "@/config/site";
+import { getAllPosts } from "@/lib/loadMDX";
+import { absoluteUrl, cn, formatDate } from "@/lib/utils";
+import { Post } from "@/types";
+import { serialize } from "next-mdx-remote/serialize";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import React from "react";
+import rehypeHighlight from "rehype-highlight";
+
+interface BlogPageProps {
+  params: {
+    slug: string[];
+  };
+}
+
+async function getPageFromParams(params: { slug: string[] }) {
+  const slug = params?.slug[0];
+  const post = getAllPosts().find((post: Post) => post.slug === slug);
+
+  if (!post) {
+    null;
+  }
+
+  const mdxSource = await serialize(post.content, {
+    mdxOptions: {
+      rehypePlugins: [rehypeHighlight],
+    },
+  });
+
+  return { ...post, mdxSource };
+}
+
+export async function generateMetadata({ params }: BlogPageProps) {
+  const page = await getPageFromParams(params);
+
+  if (!page) {
+    return {};
+  }
+
+  const url = process.env.NEXT_PUBLIC_APP_URL;
+  const ogUrl = new URL(`${url}/api/og`);
+
+  ogUrl.searchParams.set("heading", page.title);
+  ogUrl.searchParams.set("type", siteConfig.name);
+
+  return {
+    title: page.title,
+    description: page.description,
+    openGraph: {
+      title: page.title,
+      description: page.description,
+      type: "article",
+      url: absoluteUrl(page.slug),
+      images: [
+        {
+          url: ogUrl.toString(),
+          width: 1200,
+          height: 630,
+          alt: page.title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: page.title,
+      description: page.description,
+      images: [ogUrl.toString()],
+    },
+  };
+}
+
+export async function generateStaticParams() {
+  const allPosts = getAllPosts();
+  return allPosts.map((post: Post) => {
+    return {
+      slug: [post.slug],
+    };
+  });
+}
+
+const BlogPage = async ({ params }: BlogPageProps) => {
+  const post = await getPageFromParams(params);
+
+  if (!post) {
+    notFound();
+  }
+
+  const authors = post.authors.map((author) =>
+    allAuthors.find(({ slug }) => slug === `/authors/${author}`)
+  );
+  return (
+    <article className="container relative max-w-3xl py-6 lg:py-10">
+      <Link
+        href="/blog"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute left-[-200px] top-14 hidden xl:inline-flex"
+        )}
+      >
+        <Icons.backArrow className="mr-2 h-4 w-4" />
+        See all posts
+      </Link>
+      <div className="space-y-4">
+        {post.date && (
+          <time
+            dateTime={post.date}
+            className="block text-sm text-muted-foreground"
+          >
+            Published on {formatDate(post.date)}
+          </time>
+        )}
+        <h1 className="mt-2 inline-block font-heading text-4xl leading-tight lg:text-5xl">
+          {post.title}
+        </h1>
+        {post.description && (
+          <p className="text-xl text-muted-foreground">{post.description}</p>
+        )}
+        {authors?.length ? (
+          <div className="mt-4 flex space-x-4">
+            {authors.map((author) =>
+              author ? (
+                <Link
+                  key={author._id}
+                  href={`https://twitter.com/${author.twitter}`}
+                  className="flex items-center space-x-2 text-sm"
+                >
+                  <Image
+                    src={author.avatar}
+                    alt={author.title}
+                    width={42}
+                    height={42}
+                    className="rounded-full bg-white"
+                  />
+                  <div className="flex-1 text-left leading-tight">
+                    <p className="font-medium">{author.title}</p>
+                    <p className="text-[12px] text-muted-foreground">
+                      @{author.twitter}
+                    </p>
+                  </div>
+                </Link>
+              ) : null
+            )}
+          </div>
+        ) : null}
+      </div>
+      <hr className="my-4" />
+      <MdxSection code={post.mdxSource} />
+    </article>
+  );
+};
+
+export default BlogPage;
