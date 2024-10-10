c0c66f952c06d0f24071d79fb6b8676752be0603
Astra <abhaypratapsinghwd@gmail.com>
2024-10-10 15:32:33 +0530

    Blue Theme:Default Theme

--- app/(docs)/docs/[[...slug]]/page.tsx
+++ app/(docs)/docs/[[...slug]]/page.tsx
@@ -3,6 +3,7 @@ import MdxSection from "@/components/MdxSection";
 import { DocsPager } from "@/components/pager";
 import { DashboardTableOfContents } from "@/components/table-of-contents";
 import { getAllDocs } from "@/lib/fetchMDX";
+import { getMdxSource } from "@/lib/mdx";
 import { getTableOfContents } from "@/lib/toc";
 import { absoluteUrl } from "@/lib/utils";
 import { notFound } from "next/navigation";
@@ -91,9 +92,9 @@ const DocPage = async ({ params }: DocPageProps) => {
     <main className="relative py-6 lg:gap-10 lg:py-10 xl:grid xl:grid-cols-[1fr_300px]">
       <div className="mx-auto w-full min-w-0">
         <DocsPageHeader heading={doc.title} text={doc.description} />
-        {/* <MdxSection code={doc.content} /> */}
+        <MdxSection code={await getMdxSource(doc.content)} />
         <hr className="my-4 md:my-6" />
-        {/* <DocsPager doc={doc} /> */}
+        <DocsPager doc={doc} />
       </div>
       <div className="hidden text-sm xl:block">
         <div className="sticky top-16 -mt-10 max-h-[calc(var(--vh)-4rem)] overflow-y-auto pt-10">
