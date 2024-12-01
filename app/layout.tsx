765a4ba327fdc53caeddb251a218b77cbccb1f43
Astra <abhaypratapsinghwd@gmail.com>
2024-12-02 03:09:15 +0530

    Reimagining DevLogix as CodeAnalyzeX

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,8 @@
-import { TailwindIndicator } from "@/components/Tailwind-Indicator";
-import { GridSmallBackground } from "@/components/ui/GridSmallBackground";
-import { Toaster } from "@/components/ui/toaster";
-import { siteConfig } from "@/config/site";
-import type { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 
 const inter = Outfit({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: siteConfig.name,
-  description: siteConfig.description,
-};
 
 export default function RootLayout({
   children,
@@ -24,12 +14,7 @@ export default function RootLayout({
       <body
         className={`min-h-screen bg-background antialiased relative ${inter.className}`}
       >
-        <SessionProvider>
-          <GridSmallBackground />
-          {children}
-          <Toaster />
-          <TailwindIndicator />
-        </SessionProvider>
+        <SessionProvider>{children}</SessionProvider>
       </body>
     </html>
   );
