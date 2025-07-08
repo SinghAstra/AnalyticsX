81281ed84b4cdd739e1d3d27ca29740bf7eeda5a
Astra <abhaypratapsinghwd@gmail.com>
2025-07-08 14:06:41 +0530

    Rebuiilding Project....

--- lib/cookies/keys.ts
+++
@@ -1,6 +0,0 @@
-export const COOKIE_KEYS = {
-  SELECTED_CHAT_MODEL: "open-selected-chat-model",
-  IMAGE_GENERATION_MODEL: "image-generation-model",
-  USE_NATIVE_SEARCH: "use-native-search",
-  TOOLKIT: "toolkit",
-};
