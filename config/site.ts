7aec8a66221bedc84edd203d6e98c7609b6beebc
Astra <abhaypratapsinghwd@gmail.com>
2025-07-11 21:34:36 +0530

    Not Found

--- config/site.ts
+++ config/site.ts
@@ -4,7 +4,7 @@ export const siteConfig: SiteConfig = {
   name: "AnalyticsX",
   description:
     "AnalyticsX empowers you to understand your users like never before. Track page views, session times, referral sources, and user interactions with ease.",
-  url: "https://summaryx.vercel.app",
+  url: "https://analyticsx.vercel.app",
   links: {
     twitter: "https://x.com/XSinghAstra",
     githubRepo: "https://github.com/SinghAstra/analyticsx",
